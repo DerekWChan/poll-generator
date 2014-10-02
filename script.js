a4c710e2177aa66bd0670323391d999ca033443d
DerekWChan <derekchan7@gmail.com>
2014-10-02 16:38:25 -0400

    Added 'Clear Fields' functionality

--- script.js
+++ script.js
@@ -3,19 +3,21 @@ $(document).ready(function () {
     $("#generatePoll").click(function () {
         var pollName = $("#pollNameInput").val();
         var pollChoices = $("#pollChoiceInput").val().split('\n');
-            if (pollExists) {
-                $("#testPoll").remove();
-                pollExists = false;
-            }
-            $("#testArea").append("<div id=\"testPoll\"></div>");
-            $("#testPoll").append(pollName + "<br>");
-            $("#testPoll").append("<select id=\"pollChoices\"><option value=\"defaultChoice\">Select...</option></select>");
-            for (choice in pollChoices) {
-                $("#pollChoices").append("<option>" + pollChoices[choice] + "</option>");
-            }
-            pollExists = true;
+        if (pollExists) {
+            $("#testPoll").remove();
+            pollExists = false;
+        }
+        $("#testArea").append("<div id=\"testPoll\"></div>");
+        $("#testPoll").append(pollName + "<br>");
+        $("#testPoll").append("<select id=\"pollChoices\"><option value=\"defaultChoice\">Select...</option></select>");
+        for (choice in pollChoices) {
+            $("#pollChoices").append("<option>" + pollChoices[choice] + "</option>");
+        }
+        $("#testPoll").append("<input id=\"submitAnswer\" type=\"submit\" value=\"Answer\"></input>");
+        pollExists = true;
     });
     $("#clearFields").click(function () {
-        // UNDER CONSTRUCTION TODO BLAH BLAH BLAH
+        $("#pollNameInput").val("");
+        $("#pollChoiceInput").val("");
     });
 });

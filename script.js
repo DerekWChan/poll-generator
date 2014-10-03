417bc93259dc6eab69972a70ead79c58dcd63fd3
DerekWChan <derekchan7@gmail.com>
2014-10-03 15:50:34 -0400

    Updated style.css with a nicer-looking interface

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 $(document).ready(function () {
     var pollExists = false;
-    $("#generatePoll").click(function () {
+
+    // Generate Poll Button
+    $("#generatePoll").click(function() {
         var pollName = $("#pollNameInput").val();
         var pollChoices = $("#pollChoiceInput").val().split('\n');
-        if (pollExists) {
-            $("#testPoll").remove();
-            pollExists = false;
-        }
-        $("#testArea").append("<div id=\"testPoll\"></div>");
-        $("#testPoll").append(pollName + "<br>");
-        $("#testPoll").append("<select id=\"pollChoices\"><option value=\"defaultChoice\">Select...</option></select>");
-        for (choice in pollChoices) {
-            $("#pollChoices").append("<option>" + pollChoices[choice] + "</option>");
-        }
-        $("#testPoll").append("<input id=\"submitAnswer\" type=\"submit\" value=\"Answer\"></input>");
-        pollExists = true;
+        
+            if (pollExists) {
+                $("#testPoll").remove();
+                pollExists = false;
+            }
+            $("#testArea").append("<div id=\"testPoll\"></div>");
+            $("#testPoll").append(pollName + "<br>");
+            $("#testPoll").append("<select id=\"pollChoices\"><option value=\"defaultChoice\">Select...</option></select>");
+            for (choice in pollChoices) {
+                $("#pollChoices").append("<option>" + pollChoices[choice] + "</option>");
+            }
+            $("#testPoll").append("<input id=\"answerButton\" type=\"submit\" value=\"Answer\">");
+            pollExists = true;        
     });
-    $("#clearFields").click(function () {
+
+    // Clear Fields Button
+    $("#clearFields").click(function() {
         $("#pollNameInput").val("");
         $("#pollChoiceInput").val("");
+    });
+
+    // Answer Button
+    $("#answerButton").click(function() {
+        alert("Hello World");
+        //var selectedAnswer = $("#pollChoices").options[pollChoices.selectedIndex].text;
+        //$("#testArea").append(selectedAnswer);
     });
 });
